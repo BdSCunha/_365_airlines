daef0904cdf49be5ce78738aed4ce63b5ef1cf73
Bruno da Silva Cunha <bruno.cunha2@external.stellantis.com>
2023-07-13 08:51:55 -0300

    Initial commit

---
+++ src/main/java/br/org/sesisenai/estudante/_365_airlines/_365Airlines.java
@@ -0,0 +1,13 @@
+package br.org.sesisenai.estudante._365_airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _365Airlines {
+
+	public static void main(String[] args) {
+		SpringApplication.run(_365Airlines.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/sesisenai/estudante/_365_airlines/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.sesisenai.estudante._365_airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
